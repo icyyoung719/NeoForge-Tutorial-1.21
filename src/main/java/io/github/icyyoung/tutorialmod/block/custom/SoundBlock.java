5df4cc0b3e37f733130d354cf241d573be4e2279
yangege7666666 <1073182632@qq.com>
2024-09-05 20:33:09 +0800

    SoundBlock：失败，无法覆盖use方法

---
+++ src/main/java/io/github/icyyoung/tutorialmod/block/custom/SoundBlock.java
@@ -0,0 +1,29 @@
+package io.github.icyyoung.tutorialmod.block.custom;
+
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+
+/**
+ * @Description SoundBlock的模板类
+ * @Author icyyoung
+ * @Date 2024/9/5
+ */
+
+public class SoundBlock extends Block {
+    public SoundBlock(Properties properties) {
+        super(properties);
+    }
+
+    Blocks.CHEST
+//    @Override
+//    public BlockBehaviour
+//
+//    @Override
+//    public InteractionResult useW(BlockState pState, Level pLevel, BlockPos pPos,
+//                                 Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+//        pLevel.playSound(pPlayer, pPos, SoundEvents.NOTE_BLOCK_DIDGERIDOO.value(), SoundSource.BLOCKS, 1.0f, 1.0f);
+//        return InteractionResult.SUCCESS;
+//    }
+    public InteractionResult useWithoutItem
+}
