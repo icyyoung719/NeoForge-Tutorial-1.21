7412d26dee706a291d55eeb57244ec36110b97a3
icyyoung719 <1073182632@qq.com>
2025-02-17 14:11:07 +0800

    feat: Add gecko entity spawn placement and biome modifier

--- src/main/java/io/github/icyyoung/tutorialmod/worldgen/ModBiomeModifiers.java
+++ src/main/java/io/github/icyyoung/tutorialmod/worldgen/ModBiomeModifiers.java
@@ -1,6 +1,7 @@
 package io.github.icyyoung.tutorialmod.worldgen;
 
 import io.github.icyyoung.tutorialmod.TutorialMod;
+import io.github.icyyoung.tutorialmod.entity.ModEntities;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
@@ -8,10 +9,13 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.neoforged.neoforge.common.world.BiomeModifier;
 import net.neoforged.neoforge.common.world.BiomeModifiers;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+import java.util.List;
 
 /**
  * @Description TODO
@@ -25,6 +29,9 @@ public class ModBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_END_SAPPHIRE_ORE = registerKey("add_end_sapphire_ores");
 
     public static final ResourceKey<BiomeModifier> ADD_TREE_BLOODWOOD = registerKey("add_tree_bloodwood");
+
+    public static final ResourceKey<BiomeModifier> SPAWN_GECKO = registerKey("spawn_gecko");
+
 
     public static void bootstrap (BootstrapContext<BiomeModifier> context) {
         var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
@@ -52,6 +59,9 @@ public class ModBiomeModifiers {
                 HolderSet.direct(biomes.getOrThrow(Biomes.PLAINS), biomes.getOrThrow(Biomes.SAVANNA)),
                 HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.BLOODWOOD_PLACED_KEY)),
                 GenerationStep.Decoration.VEGETAL_DECORATION));
+        context.register(SPAWN_GECKO, new BiomeModifiers.AddSpawnsBiomeModifier(
+                HolderSet.direct(biomes.getOrThrow(Biomes.SWAMP), biomes.getOrThrow(Biomes.PLAINS)),
+                List.of(new MobSpawnSettings.SpawnerData(ModEntities.GECKO.get(), 20, 2,4))));
     }
 
     public static ResourceKey<BiomeModifier> registerKey(String name) {
